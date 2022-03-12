727b4b1597af247903a0c9df5a890fd9b53ab762
bilginyuksel <bilgin.yuksel96@gmail.com>
2022-03-13 01:22:20 +0300

    Update main.go with broker

--- main.go
+++ main.go
@@ -6,14 +6,25 @@ import (
 	"os/signal"
 	"time"
 
+	"github.com/bilginyuksel/mque/internal/broker"
 	"github.com/bilginyuksel/mque/internal/conn"
 )
 
 func main() {
+	b := broker.New()
 	s := conn.Server{
-		OnConnection: func(c conn.Conn) {
-			log.Printf("accepted new connection with id: %v\n\n", c)
+		OnConnection: func(c *conn.Conn) {
+			// log.Printf("accepted new connection with id: %v\n\n", c)
+			switch c.Conf.Type {
+			case conn.Publisher:
+				b.Publish(c.Conf.Topic, c)
+				log.Println("created a publisher")
+			case conn.Subscriber:
+				b.Subscribe(c.Conf.Topic, c)
+				log.Println("created a subscriber")
+			}
 		},
+		Connections:             make(map[string]*conn.Conn),
 		ConnectionWriteDeadline: 2 * time.Second,
 		ConnectionReadDeadline:  2 * time.Second,
 	}
