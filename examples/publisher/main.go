422a8d326655c9b77ab01268eccd54cdcff5cd38
bilginyuksel <bilgin.yuksel96@gmail.com>
2022-03-14 00:38:20 +0300

    Update examples with the new mq package

--- examples/publisher/main.go
+++ examples/publisher/main.go
@@ -1,75 +1,19 @@
 package main
 
 import (
-	"bufio"
-	"encoding/json"
-	"log"
-	"net"
-	"os"
+	"github.com/bilginyuksel/mque/pkg/mq"
 )
 
 func main() {
-	con, err := net.Dial("tcp", ":8080")
+	writer, err := mq.NewWriter(mq.WriterConfig{
+		URL:         ":8080",
+		Topic:       "test",
+		MaxByteSize: 20000,
+		MinByteSize: 1,
+	})
 	if err != nil {
 		panic(err)
 	}
 
-	info := make([]byte, 1024)
-	if _, err := con.Read(info); err != nil {
-		panic(err)
-	}
-
-	log.Println("received value from server:", string(info))
-
-	conf := Config{
-		Topic:        "test",
-		ReadTimeout:  1000,
-		WriteTimeout: 2000,
-		MaxByteSize:  33223232,
-		MinByteSize:  1,
-		Type:         Publisher,
-	}
-	confBytes, _ := json.Marshal(conf)
-	if _, err := con.Write(confBytes); err != nil {
-		panic(err)
-	}
-
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for {
-		var msgContent string
-		if scanner.Scan() {
-			msgContent = scanner.Text()
-		}
-
-		if msgContent == "q" {
-			break
-		}
-
-		log.Println("sending message:", msgContent)
-
-		if _, err := con.Write([]byte(msgContent)); err != nil {
-			log.Println("write failed, err:", err)
-		}
-	}
-
-	if err := con.Close(); err != nil {
-		panic(err)
-	}
-}
-
-const (
-	Publisher = iota + 1
-	Subscriber
-)
-
-type Config struct {
-	Topic        string `json:"topic"`
-	ReadTimeout  int64  `json:"read_timeout"`
-	WriteTimeout int64  `json:"write_timeout"`
-	MaxByteSize  int64  `json:"max_byte_size"`
-	MinByteSize  int64  `json:"min_byte_size"`
-	Type         uint8  `json:"type"`
-
-	// Encryption
+	writer.Write()
 }
--- examples/singlecon/main.go
+++ examples/singlecon/main.go
@@ -1,78 +1,19 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
-	"net"
-	"os"
-	"os/signal"
+	"github.com/bilginyuksel/mque/pkg/mq"
 )
 
 func main() {
-	con, err := net.Dial("tcp", ":8080")
+	reader, err := mq.NewReader(mq.ReaderConfig{
+		URL:         ":8080",
+		Topic:       "test",
+		MaxByteSize: 232322,
+		MinByteSize: 1,
+	})
 	if err != nil {
 		panic(err)
 	}
 
-	info := make([]byte, 1024)
-	if _, err := con.Read(info); err != nil {
-		panic(err)
-	}
-
-	log.Println("received value from server:", string(info))
-
-	conf := Config{
-		Topic:        "test",
-		ReadTimeout:  1000,
-		WriteTimeout: 2000,
-		MaxByteSize:  33223232,
-		MinByteSize:  1,
-		Type:         Subscriber,
-	}
-	confBytes, _ := json.Marshal(conf)
-	if _, err := con.Write(confBytes); err != nil {
-		panic(err)
-	}
-
-	go func() {
-		chunk := make([]byte, 2048)
-		for {
-			msgByteLength, err := con.Read(chunk)
-			if err != nil {
-				log.Printf("msg read failed, err: %v\n", err)
-				break
-			}
-
-			msg := make([]byte, msgByteLength)
-			copy(msg, chunk)
-
-			log.Printf("length: %d, msg: %s\n", msgByteLength, string(msg))
-		}
-	}()
-
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, os.Interrupt)
-	<-quit
-
-	log.Println("shutting down")
-
-	if err := con.Close(); err != nil {
-		panic(err)
-	}
-}
-
-const (
-	Publisher = iota + 1
-	Subscriber
-)
-
-type Config struct {
-	Topic        string `json:"topic"`
-	ReadTimeout  int64  `json:"read_timeout"`
-	WriteTimeout int64  `json:"write_timeout"`
-	MaxByteSize  int64  `json:"max_byte_size"`
-	MinByteSize  int64  `json:"min_byte_size"`
-	Type         uint8  `json:"type"`
-
-	// Encryption
+	reader.Read()
 }
